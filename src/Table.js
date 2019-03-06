a5508b28f9d65041be5db9bdc0d464ebcd8f4964
Serhii Rudyi <SRudyi@rocketsoftware.com>
2019-03-06 18:11:27 +0200

    Add resize columns (bad working)

--- src/Table.js
+++ src/Table.js
@@ -2,35 +2,82 @@ class Table {
     constructor(schema, data) {
         this.schema = schema;
         this.data = data;
+        this._buildHTML = this._buildHTML.bind(this);
+        this._createTable = this._createTable.bind(this);
         this._createTHeader = this._createTHeader.bind(this);
         this._createTBody = this._createTBody.bind(this);
+        this._createTableWrapper = this._createTableWrapper.bind(this);
+        this._createFloatTHeader =  this._createFloatTHeader.bind(this);
+        this._onTableScroll = this._onTableScroll.bind(this);
     }
 
     static createTable(schema, data) {
-        const table = new Table(schema, data);
-        return table._createTable();
+        return new Table(schema, data)._buildHTML();
+    }
+
+    _buildHTML() {
+        
+        // Creating table header
+        this.tHead = this._createTHeader(this.schema);
+        
+        // Creating floating table header (need table header)
+        this.floatTHead = this._createFloatTHeader(this.tHead);
+        
+        // Creating table body
+        this.tBody = this._createTBody(this.schema, this.data)
+        
+        // Creating main table
+        this.table = this._createTable();
+        this.table.appendChild(this.tHead);
+        this.table.appendChild(this.tBody);
+
+        // Creating table-wrapper
+        this.tableWrapper = this._createTableWrapper();
+        this.tableWrapper.appendChild(this.floatTHead);
+        this.tableWrapper.appendChild(this.table);
+
+        this.tableWrapper.addEventListener('scroll', this._onTableScroll);
+        
+        return this.tableWrapper
     }
 
     _createTable() {
         const table = document.createElement('table');
-        const thead = this._createTHeader(this.schema);
-        const tbody = this._createTBody(this.schema, this.data)
-        table.appendChild(thead);
-        table.appendChild(tbody);
+        table.classList.add("scrolled_table");
+        
         return table
+    }
+
+    _createTableWrapper() {
+        const tableWrapper = document.createElement('div');
+        tableWrapper.classList.add("table_wrapper");
+
+        return tableWrapper
     }
     
     _createTHeader(schema) {
         const thead = document.createElement("thead");
         const tr = document.createElement("tr");
         schema.forEach(col => {
+            const separator = this._createTableSeparator(100);
             const th = document.createElement("th");
             th.innerText = col.headerName;
-            th.width = col.width;
+            th.style.width = col.width;
+            th.appendChild(separator);
             tr.appendChild(th);        
         });
         thead.appendChild(tr);
+
         return thead;
+    }
+
+    _createFloatTHeader(thead) {
+        const table = document.createElement('table');
+        const floatTHead = thead.cloneNode(true);
+        table.classList.add("table_float_header");
+        table.appendChild(floatTHead);
+
+        return table
     }
     
     _createTBody(schema, data) {
@@ -46,7 +93,54 @@ class Table {
             });
             tbody.appendChild(tr);
         });
+
         return tbody
+    }
+
+    _onTableScroll(e) {
+        // Emulation fixed position for floating table header
+        let top = e.target.scrollTop;
+        this.floatTHead.style.top = top + "px";
+    }
+
+    _createTableSeparator(height){
+        const div = document.createElement('div');
+        div.classList.add("separator");
+        div.style.height = height + 'px';
+
+        let targetCol, nextColumn, pageX, targetColWidth, nextColumnWidth;
+
+        div.addEventListener('mousedown', e => {
+            targetCol = e.target.parentElement;
+            nextColumn = targetCol.nextElementSibling;
+            pageX = e.pageX;
+            targetColWidth = targetCol.offsetWidth;
+            nextColumnWidth = nextColumn ? nextColumn.offsetWidth : undefined;
+        });
+
+        document.addEventListener('mousemove', e => {
+
+            if (targetCol) {
+                let diff = e.pageX - pageX;
+                
+                if (nextColumn) {
+                    nextColumn.style.width = (nextColumnWidth - diff) + 'px';
+                };
+
+                targetCol.style.width = (targetColWidth + diff) + 'px';
+            }
+        });
+
+        document.addEventListener('mouseup', e => {
+
+            targetCol = undefined;
+            nextColumn = undefined;
+            pageX = undefined;
+            targetColWidth = undefined;
+            nextColumnWidth = undefined
+        });
+
+        return div;
     }
 }
 
--- src/scheme.js
+++ src/scheme.js
@@ -1,37 +1,15 @@
 const schema = [
-  {
-    dataName: "short_description", headerName: "Recommended Action", width: "198px"
-  },
-  {
-    dataName: "target_qualifier", headerName: "Database Name", width: "125px"
-  },
-  {
-    dataName: "target_type", headerName: "Resource Type", width: "120px"
-  },
-  {
-    dataName: "target_object", headerName: "TS / IS Name", width: "102px"
-  },
-  {
-    dataName: "target_partition", headerName: "Partition", width: "93px"
-  },
-  {
-    dataName: "status", headerName: "Status", width: "117px"
-  },
-  {
-    dataName: "autonomic_type", headerName: "Autonomic Type", width: "102px"
-  },
-  {
-    dataName: "priority_override_int", headerName: "Priority Override", width: "94px"
-  },
-  {
-    dataName: "priority", headerName: "Priority", width: "144px"
-  },
-  {
-    dataName: "window_name", headerName: "Window Name", width: "103px"
-  },
-  {
-    dataName: "startts", headerName: "Modified on", width: "103px"
-  }    
+  { dataName: "short_description", headerName: "Recommended Action", width: "160px" },
+  { dataName: "target_qualifier", headerName: "Database Name", width: "125px" },
+  { dataName: "target_type", headerName: "Resource Type", width: "110px" },
+  { dataName: "target_object", headerName: "TS / IS Name", width: "102px" },
+  { dataName: "target_partition", headerName: "Partition", width: "100px" },
+  { dataName: "status", headerName: "Status", width: "100px" },
+  { dataName: "autonomic_type", headerName: "Autonomic Type", width: "115px" },
+  { dataName: "priority_override_int", headerName: "Priority Override", width: "100px" },
+  { dataName: "priority", headerName: "Priority", width: "100px" },
+  { dataName: "window_name", headerName: "Window Name", width: "145px" },
+  { dataName: "startts", headerName: "Modified on", width: "120px" }    
 ]
 
 export default schema;
